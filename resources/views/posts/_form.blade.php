3f3344e121470048137da5996eeb5ed74dd0b9d4
underwookie <galaxywookie@gmail.com>
2020-05-02 13:11:59 -0700

    After Section 3

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,16 +1,15 @@
-<p>
+<div class="form-group">
     <label for="">Title</label>
-    <input type="text" name="title"
+    <input type="text" name="title" class="form-control"
         value="{{ old('title', $post->title ?? null) }}"/>
-</p>
+</div>
 
-<p>
+<div class="form-group">
     <label for="">Content</label>
-    <input type="text" name="content"
+    <input type="text" name="content"  class="form-control"
         value="{{ old('content', $post->content ?? null) }}"/>
 
-</p>
-
+</div>
 @if($errors->any())
     <div>
         <ul>
